8d6785389d72b141b05e5a3bc752b44bef806b29
Anastasiev Slava <slava2221@gmail.com>
2017-11-21 16:31:30 +0000

    many changes to make posible add peers

--- eth/scripts/run.sh
+++ eth/scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-geth --mine --minerthreads 1 --identity "SberBankNode" --datadir "chains/devtest/" --port "30303" --rpcapi "db,eth,net,web3" --nodiscover --rpc --rpcport "8545"  --rpcaddr="172.18.0.2" --rpccorsdomain "*" --syncmode "fast" --nousb --networkid 552
+geth --mine --minerthreads 1 --identity "SberBankNode" --datadir "chains/devtest/" --port "30303" --rpcapi "db,eth,net,web3" --nodiscover --rpc --rpcport "8545"  --rpcaddr="172.19.0.2" --rpccorsdomain "*" --networkid 552
